511800487ea988268bca8217c7c38bb36502528d
Hemanth <2300032944@kluniversity.in>
2025-07-28 10:32:54 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - KIRAN</h1>
-      <p>CI/CD cloud devops</p>
+      <p><b>CI/CD devops 2025</b></p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
